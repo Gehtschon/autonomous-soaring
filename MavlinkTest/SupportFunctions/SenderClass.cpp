cc6572b739c8855882f899cac32d39d7091e58ab
Fabian Glutz <fabian.glutz@students.fhnw.ch>
2023-11-14 22:48:44 +0100

    Mav_Request_Data finished not testedgit commit

--- MavlinkTest/SupportFunctions/SenderClass.cpp
+++ MavlinkTest/SupportFunctions/SenderClass.cpp
@@ -4,9 +4,43 @@
 
 #include "SenderClass.h"
 
-SenderClass::SenderClass(int socketFd) : socket_fd(socketFd) {
-    src_addr_len = sizeof(src_addr);
-
-    Mav_Request_Data(socket_fd, &src_addr, src_addr_len);
+SenderClass::SenderClass(int socketFd) {
+     src_addr_len = sizeof(src_addr);
+    this->socket_fd  = socketFd;
+     // here start to request data
+    //void Mav_Request_Data(uint8_t MAVStreams[],int16_t MAVRates[]);
 };
 
+
+
+
+uint8_t SenderClass::Mav_Request_Data(uint8_t MAVStreams[],int16_t MAVRates[],int MavStreamSize, int MavRateSize){
+
+    uint8_t retVal = 99;
+    bool fail = false;
+    mavlink_message_t msg;
+    uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+    if(MavStreamSize != MavRateSize){
+        return 99;
+    }
+
+    for (int i = 0; i < MavStreamSize; ++i) {
+        mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
+        int16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+        int ret = sendto(socket_fd, buf, len, 0, (const struct sockaddr*)&this->src_addr, src_addr_len);
+
+        if (ret != len){
+            fail = true;
+        }
+
+    }
+
+    if (fail == true){
+        return 1;
+    } else{
+        return 0;
+    }
+
+
+}
--- MavlinkTest/SupportFunctions/SenderClass.h
+++ MavlinkTest/SupportFunctions/SenderClass.h
@@ -19,7 +19,24 @@ private:
 public:
     explicit SenderClass(int socketFd);
     void send_some(socklen_t src_addr_len,mavlink_message_t message);
-    void Mav_Request_Data(uint8_t MAVStreams[],int16_t MAVRates[]); // check in function sizeof(MAVStreams) must equal to sizeof(MAVRates)
+
+
+    /**
+ * Activating datastreams on flightcontroller
+ *
+ * Activating datastreams on flighcontroller. The 2 Arrays need tro be same size!
+ * use this function to activate datastreams that should be periodicly updatet
+ *
+ * @param MAVStreams Wich Streams are activated more infos in common.h: enum MAV_DATA_STREAM
+ * @param MAVRates Datarates of the Streams
+ * @param MavStreamSize Size of the MAVStreams Array
+ * @param MavStreamSize Size of the MAVRates Array
+ * @return
+ *   - 99: Different array size
+ *   -  1: Failed to send
+ *   -  0: No error
+ */
+    uint8_t Mav_Request_Data(uint8_t MAVStreams[],int16_t MAVRates[],int MavStreamSize, int MavRateSize); // check in function sizeof(MAVStreams) must equal to sizeof(MAVRates)
     // this function is implemented in main, rewrite it here
 };
 
